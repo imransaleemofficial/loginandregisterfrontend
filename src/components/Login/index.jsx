6bf595fe0069a7c98de44d3fe36d27548dd29741
imransaleemofficial <imransaleem987789@gmail.com>
2023-07-17 01:27:43 +0500

    first commit

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -15,6 +15,7 @@ const Login = () => {
 			const url = "http://localhost:8080/api/auth";
 			const { data: res } = await axios.post(url, data);
 			localStorage.setItem("token", res.data);
+			alert("you are logged in successfully")
 			window.location = "/";
 		} catch (error) {
 			if (
